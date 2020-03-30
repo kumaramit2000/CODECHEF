fa962de8871b944d8a2854c3c549ee0c4ba702a4
kumaramit2000 <47832506+kumaramit2000@users.noreply.github.com>
2020-03-30 23:41:02 +0530

    Create ZCO14003.cpp

---
+++ DSA Learning Series/1 - Complexity Analysis + Basics Warm Up/ZCO14003.cpp
@@ -0,0 +1,30 @@
+/*love_for_code*/
+/*code_for_love*/
+//Code_written_by: @AMIT KUMAR;
+#define ll long long int
+#define ull unsigned long long int
+#define MODM 1000000007
+#define MODS 10000007
+#define INF 2000000000000000000LL
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n;
+    cin>>n;
+    ll a[n];
+    ll ans=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i=0;i<n;i++)
+    {
+        ll temp=a[i]*(n-i);
+        ans=max(ans,temp);
+    }
+    cout<<ans<<endl;
+	return 0;
+}
